e029a02d530999b0ffb339ec1227048b42a42b7a
MolinaFranco <franco13mm@gmail.com>
2023-04-06 17:59:03 -0300

    actualizando a submodulos

---
+++ LabODC/ejercicio-2/app.s
@@ -0,0 +1,191 @@
+
+.equ SCREEN_WIDTH, 		640
+.equ SCREEN_HEIGH, 		480
+.equ BITS_PER_PIXEL,  	32
+
+
+.globl main
+main:
+	mov		x15, 720	// posicion x0 del meteorito
+	mov		x16, 1		// posicion y0 del meteorito
+	mov		x19, 1
+	mov		x24, 8		//cantidad de cactus
+	mov 	x25, 610 	//posicion del cactus x0
+	mov 	x26, 250 	//posicion del dino y0
+	mov		x27, 410	// posicion de la nuve
+	mov		x29, 1		// tipo de cactus
+	mov		x14, 0		// puntos
+mainpostinit:
+
+	// dibujo fondo
+	// X0 contiene la direccion base del framebuffer - tiene el (0,0) del framebuffer
+ 	mov 	x20, x0	// Tengo que hacer un arreglo que tiene el tamaño del framebuffer 
+	bl virtual_frame.change_base_pos
+	bl virtual_frame.show_frame
+	//---------------- CODE HERE ------------------------------------
+
+	movz 	x10, 0x76, lsl 16
+	movk 	x10, 0xadd0, lsl 00
+	mov 	x1, #0
+	mov 	x2, #0
+	mov 	x3, SCREEN_WIDTH
+	mov 	x4, SCREEN_HEIGH
+	bl 		doRectangulo
+	
+
+// clouds
+
+    mov 	x1, x27
+	sub 	x27, x27, 1
+    mov 	x2, #100
+    // x10 -> Color
+	bl		doCloud
+
+	cmp		x24, 4
+	b.lt	meteorito
+endmeteorito:
+
+	mov		x1, #320
+	mov		x2, #286
+	bl		doMountain
+
+	movz 	x10, 0xdf, lsl 16
+	movk 	x10, 0xae94, lsl 00
+	mov 	x1, #0
+	mov 	x2, #330
+	mov 	x3, SCREEN_WIDTH
+	mov 	x4, 330
+	bl 		doGradiente
+	
+
+// cactus
+
+	movz 	x10, 0x00, lsl 16
+	movk 	x10, 0xbb2d, lsl 00
+	
+	cbz		x24, ultimocactus
+	sub 	x25, x25, 5	//muevo el cuadrado proximamente cactus -velocity-
+endultic:
+	
+	cmp		x25, xzr 
+	b.le	resetcactus
+
+	mov 	x1, x25
+	mov 	x2, #250
+	
+	mov		x18, x29
+	bl 		doCactusT
+
+
+// movimiento dino
+
+	cbz		x24, ultimodino 
+
+	cmp		x25, 200
+	b.lt	bajardino
+	cmp		x25, 400
+	b.lt	subirdino
+
+endydino:
+	
+
+// dibujar dino
+
+	add 	x19, x19, 1
+	cmp 	x19, #4
+	b.eq 	resetdino
+
+doDino:
+	mov 	x1, #200
+	mov 	x2, x26
+	bl doDinoT
+
+	//---------------------------------------------------------------
+	// Infinite Loop
+
+	mov 	x28, 1
+	lsl 	x28,x28,21	//velocity
+	b 		delay
+
+	//b		main
+
+EndLopp:
+	b EndLopp
+
+
+
+
+
+
+
+
+
+
+
+resetdino:
+	mov		x19, 1
+	ret
+
+delay:
+	add 	xzr,xzr,xzr
+	subs 	x28, x28, 1
+	cbnz 	x28, delay
+
+	b 		mainpostinit
+
+subirdino:
+	sub		x26, x26, 2
+	b		endydino
+
+bajardino:
+	cmp		x26, 250
+	b.lt	bajardinoreal
+	b		endydino
+
+bajardinoreal:
+	add		x26, x26, 3
+	b		endydino
+
+resetcactus:
+	mov 	x25, 610
+	sub 	x24, x24, 1
+	add 	x14, x14, 1
+    add     x29, x29, 1
+
+    cmp     x29, 4
+    b.lt    cacmincua	
+    mov     x29, 1
+cacmincua:    
+    ret
+
+ultimocactus:
+	cmp		x25, #270
+	b.eq 	endultic
+	sub 	x25, x25, 5
+	b 		endultic
+
+ultimodino:	
+	cmp 	x25, #270
+	b.ne	doDino
+	mov 	x19, 4
+	b		doDino
+
+meteorito:
+	mov 	x1, x15
+	mov 	x2, x16
+	bl 		doMeteorito
+	sub		x15, x15, 4
+	add		x16, x16, 2
+	cmp		x15, -600
+	b.lt	resetmeteorito
+	
+	b		endmeteorito
+
+
+resetmeteorito:
+	mov		x15, 720	// posicion x0 del meteorito
+	mov		x16, 1		// posicion y0 del meteorito
+	b		endmeteorito
+
+
+
---
+++ LabODC/ejercicio-2/numeros.s
@@ -0,0 +1,464 @@
+.equ NUMBER_COLOR1, 0x00
+.equ NUMBER_COLOR2, 0x0000
+
+
+
+n_0:
+
+    sub 	sp,sp, #32    
+	str 	x10, [sp, #24]   
+	str 	x2, [sp, #16]   
+	str 	x1, [sp, #8]   
+	str 	x30, [sp, #0]   
+	
+    mov 	x8, x1
+	mov 	x9, x2
+
+	add 	x1, x8, #0
+	add 	x2, x9, #0
+	mov 	x3, #12
+	mov 	x4, #4
+	bl 		doRectangulo
+	add 	x1, x8, #0
+	add 	x2, x9, #12
+	mov 	x3, #12
+	mov 	x4, #4
+	bl 		doRectangulo
+	add 	x1, x8, #0
+	add 	x2, x9, #0
+	mov 	x3, #4
+	mov 	x4, #16
+	bl 		doRectangulo
+	add 	x1, x8, #8
+	add 	x2, x9, #0
+	mov 	x3, #4
+	mov 	x4, #16
+	bl 		doRectangulo
+
+	ldr 	x10, [sp, #24]   
+	ldr 	x2, [sp, #16]   
+	ldr 	x1,	[sp, #8]   
+	ldr 	x30,[sp, #0] 
+	add 	sp, sp, #32
+	b 		cifra
+
+n_1:
+
+    sub 	sp,sp, #32    
+	str 	x10, [sp, #24]   
+	str 	x2, [sp, #16]   
+	str 	x1, [sp, #8]   
+	str 	x30, [sp, #0]   
+	
+    mov 	x8, x1
+	mov 	x9, x2
+
+	add 	x1, x8, #0
+	add 	x2, x9, #0
+	mov 	x3, #8
+	mov 	x4, #4
+	bl 		doRectangulo
+	add 	x1, x8, #4
+	add 	x2, x9, #0
+	mov 	x3, #4
+	mov 	x4, #20
+	bl 		doRectangulo
+	add 	x1, x8, #0
+	add 	x2, x9, #16
+	mov 	x3, #12
+	mov 	x4, #4
+	bl 		doRectangulo
+
+	ldr 	x10, [sp, #24]   
+	ldr 	x2, [sp, #16]   
+	ldr 	x1,	[sp, #8]   
+	ldr 	x30,[sp, #0] 
+	add 	sp, sp, #32
+	b 		cifra
+
+n_2:
+
+    sub 	sp,sp, #32    
+	str 	x10, [sp, #24]   
+	str 	x2, [sp, #16]   
+	str 	x1, [sp, #8]   
+	str 	x30, [sp, #0]   
+	
+    mov 	x8, x1
+	mov 	x9, x2
+
+	add 	x1, x8, #0
+	add 	x2, x9, #0
+	mov 	x3, #12
+	mov 	x4, #4
+	bl 		doRectangulo
+	add 	x1, x8, #0
+	add 	x2, x9, #8
+	mov 	x3, #12
+	mov 	x4, #4
+	bl 		doRectangulo
+	add 	x1, x8, #0
+	add 	x2, x9, #16
+	mov 	x3, #12
+	mov 	x4, #4
+	bl 		doRectangulo
+	add 	x1, x8, #8
+	add 	x2, x9, #4
+	mov 	x3, #4
+	mov 	x4, #4
+	bl 		doRectangulo
+	add 	x1, x8, #0
+	add 	x2, x9, #12
+	mov 	x3, #4
+	mov 	x4, #4
+	bl 		doRectangulo
+
+	ldr 	x10, [sp, #24]   
+	ldr 	x2, [sp, #16]   
+	ldr 	x1,	[sp, #8]   
+	ldr 	x30,[sp, #0] 
+	add 	sp, sp, #32
+	b  		cifra
+
+n_3:
+
+    sub 	sp,sp, #32    
+	str 	x10, [sp, #24]   
+	str 	x2, [sp, #16]   
+	str 	x1, [sp, #8]   
+	str 	x30, [sp, #0]   
+	
+    mov 	x8, x1
+	mov 	x9, x2
+
+	add 	x1, x8, #0
+	add 	x2, x9, #0
+	mov 	x3, #12
+	mov 	x4, #4
+	bl 		doRectangulo
+	add 	x1, x8, #4
+	add 	x2, x9, #8
+	mov 	x3, #8
+	mov 	x4, #4
+	bl 		doRectangulo
+	add 	x1, x8, #0
+	add 	x2, x9, #16
+	mov 	x3, #12
+	mov 	x4, #4
+	bl 		doRectangulo
+	add 	x1, x8, #8
+	add 	x2, x9, #4
+	mov 	x3, #4
+	mov 	x4, #4
+	bl 		doRectangulo
+	add 	x1, x8, #8
+	add 	x2, x9, #12
+	mov 	x3, #4
+	mov 	x4, #4
+	bl 		doRectangulo
+
+    ldr 	x10, [sp, #24]   
+	ldr 	x2, [sp, #16]   
+	ldr 	x1,	[sp, #8]   
+	ldr 	x30,[sp, #0] 
+	add 	sp, sp, #32
+	b 		cifra
+
+n_4:
+
+    sub 	sp,sp, #32    
+	str 	x10, [sp, #24]   
+	str 	x2, [sp, #16]   
+	str 	x1, [sp, #8]   
+	str 	x30, [sp, #0]   
+	
+    mov 	x8, x1
+	mov 	x9, x2
+
+	add 	x1, x8, #0
+	add 	x2, x9, #0
+	mov 	x3, #4
+	mov 	x4, #12
+    bl      doRectangulo
+	add 	x1, x8, #8
+	add 	x2, x9, #0
+	mov 	x3, #4
+	mov 	x4, #20
+    bl      doRectangulo
+	add 	x1, x8, #4
+	add 	x2, x9, #8
+	mov 	x3, #4
+	mov 	x4, #4
+    bl      doRectangulo
+
+    ldr 	x10, [sp, #24]   
+	ldr 	x2, [sp, #16]   
+	ldr 	x1,	[sp, #8]   
+	ldr 	x30,[sp, #0] 
+	add 	sp, sp, #32
+	b 		cifra     
+
+n_5:
+
+    sub 	sp,sp, #32    
+	str 	x10, [sp, #24]   
+	str 	x2, [sp, #16]   
+	str 	x1, [sp, #8]   
+	str 	x30, [sp, #0]   
+	
+    mov 	x8, x1
+	mov 	x9, x2
+
+	add 	x1, x8, #0
+	add 	x2, x9, #0
+	mov 	x3, #12
+	mov 	x4, #4
+    bl      doRectangulo
+	add 	x1, x8, #0
+	add 	x2, x9, #8
+	mov 	x3, #12
+	mov 	x4, #4
+    bl      doRectangulo
+	add 	x1, x8, #0
+	add 	x2, x9, #16
+	mov 	x3, #12
+	mov 	x4, #4
+    bl      doRectangulo
+	add 	x1, x8, #0
+	add 	x2, x9, #4
+	mov 	x3, #4
+	mov 	x4, #4
+    bl      doRectangulo
+	add 	x1, x8, #8
+	add 	x2, x9, #12
+	mov 	x3, #4
+	mov 	x4, #4
+    bl      doRectangulo
+
+    ldr 	x10, [sp, #24]   
+	ldr 	x2, [sp, #16]   
+	ldr 	x1,	[sp, #8]   
+	ldr 	x30,[sp, #0] 
+	add 	sp, sp, #32
+	b 		cifra     
+
+n_6:
+
+    sub 	sp,sp, #32    
+	str 	x10, [sp, #24]   
+	str 	x2, [sp, #16]   
+	str 	x1, [sp, #8]   
+	str 	x30, [sp, #0]   
+	
+    mov 	x8, x1
+	mov 	x9, x2
+
+	add 	x1, x8, #0
+	add 	x2, x9, #0
+	mov 	x3, #12
+	mov 	x4, #4
+    bl      doRectangulo
+	add 	x1, x8, #0
+	add 	x2, x9, #8
+	mov 	x3, #12
+	mov 	x4, #4
+    bl      doRectangulo
+	add 	x1, x8, #0
+	add 	x2, x9, #16
+	mov 	x3, #12
+	mov 	x4, #4
+    bl      doRectangulo
+	add 	x1, x8, #0
+	add 	x2, x9, #4
+	mov 	x3, #4
+	mov 	x4, #4
+    bl      doRectangulo
+	add 	x1, x8, #8
+	add 	x2, x9, #12
+	mov 	x3, #4
+	mov 	x4, #4
+    bl      doRectangulo
+	add 	x1, x8, #0
+	add 	x2, x9, #12
+	mov 	x3, #4
+	mov 	x4, #4
+    bl      doRectangulo
+
+    ldr 	x10, [sp, #24]   
+	ldr 	x2, [sp, #16]   
+	ldr 	x1,	[sp, #8]   
+	ldr 	x30,[sp, #0] 
+	add 	sp, sp, #32
+	b 		cifra     
+
+n_7:
+
+    sub 	sp,sp, #32    
+	str 	x10, [sp, #24]   
+	str 	x2, [sp, #16]   
+	str 	x1, [sp, #8]   
+	str 	x30, [sp, #0]   
+	
+    mov 	x8, x1
+	mov 	x9, x2
+
+	add 	x1, x8, #0
+	add 	x2, x9, #0
+	mov 	x3, #12
+	mov 	x4, #4
+    bl      doRectangulo
+	add 	x1, x8, #8
+	add 	x2, x9, #0
+	mov 	x3, #4
+	mov 	x4, #20
+    bl      doRectangulo
+	
+    ldr 	x10, [sp, #24]   
+	ldr 	x2, [sp, #16]   
+	ldr 	x1,	[sp, #8]   
+	ldr 	x30,[sp, #0] 
+	add 	sp, sp, #32
+	b		cifra     
+
+n_8:
+
+    sub 	sp,sp, #32    
+	str 	x10, [sp, #24]   
+	str 	x2, [sp, #16]   
+	str 	x1, [sp, #8]   
+	str 	x30, [sp, #0]   
+	
+    mov 	x8, x1
+	mov 	x9, x2
+
+	add 	x1, x8, #0
+	add 	x2, x9, #0
+	mov 	x3, #12
+	mov 	x4, #4
+    bl      doRectangulo
+	add 	x1, x8, #0
+	add 	x2, x9, #8
+	mov 	x3, #12
+	mov 	x4, #4
+    bl      doRectangulo
+	add 	x1, x8, #0
+	add 	x2, x9, #16
+	mov 	x3, #12
+	mov 	x4, #4
+    bl      doRectangulo
+	add 	x1, x8, #0
+	add 	x2, x9, #4
+	mov 	x3, #4
+	mov 	x4, #4
+    bl      doRectangulo
+	add 	x1, x8, #8
+	add 	x2, x9, #12
+	mov 	x3, #4
+	mov 	x4, #4
+    bl      doRectangulo
+	add 	x1, x8, #0
+	add 	x2, x9, #12
+	mov 	x3, #4
+	mov 	x4, #4
+    bl      doRectangulo
+	add 	x1, x8, #8
+	add 	x2, x9, #4
+	mov 	x3, #4
+	mov 	x4, #4
+    bl      doRectangulo
+
+    ldr 	x10, [sp, #24]   
+	ldr 	x2, [sp, #16]   
+	ldr 	x1,	[sp, #8]   
+	ldr 	x30,[sp, #0] 
+	add 	sp, sp, #32
+	b		cifra     
+
+n_9:
+
+    sub 	sp,sp, #32    
+	str 	x10, [sp, #24]   
+	str 	x2, [sp, #16]   
+	str 	x1, [sp, #8]   
+	str 	x30, [sp, #0]   
+	
+    mov 	x8, x1
+	mov 	x9, x2
+
+	add 	x1, x8, #0
+	add 	x2, x9, #0
+	mov 	x3, #12
+	mov 	x4, #4
+    bl      doRectangulo
+	add 	x1, x8, #0
+	add 	x2, x9, #8
+	mov 	x3, #8
+	mov 	x4, #4
+    bl      doRectangulo
+	add 	x1, x8, #0
+	add 	x2, x9, #4
+	mov 	x3, #4
+	mov 	x4, #4
+    bl      doRectangulo
+	add 	x1, x8, #8
+	add 	x2, x9, #0
+	mov 	x3, #4
+	mov 	x4, #20
+    bl      doRectangulo
+
+    ldr 	x10, [sp, #24]   
+	ldr 	x2, [sp, #16]   
+	ldr 	x1,	[sp, #8]   
+	ldr 	x30,[sp, #0] 
+	add 	sp, sp, #32
+	b		cifra     
+
+cifra: 
+	
+
+
+.globl doNumber
+doNumber:
+    // utiliza los  1,2,8,9,10, 12, 13, 14, 15, 16, 17
+    // modifica x8, x9
+
+	mov x5, 0 	//utilizo para determinar en que cifra estoy
+
+    sub 	sp,sp, #32    
+	str 	x30, [sp, #0]   
+	str 	x1,  [sp, #8]   
+	str 	x2,  [sp, #16]   
+	str 	x10, [sp, #24]   
+	
+    mov 	x8, x1
+	mov 	x9, x2
+
+    movz 	x10, NUMBER_COLOR1, lsl 16
+	movk 	x10, NUMBER_COLOR2, lsl 00
+
+
+    mov     x18, 4
+    mov     x19, 8
+
+
+    cmp		x18, #0
+	b.eq	n_0 	 
+    cmp		x18, #1
+	b.eq	n_1 	 
+    cmp		x18, #2
+	b.eq	n_2 	 
+    cmp		x18, #3
+	b.eq	n_3 
+    cmp		x18, #4
+	b.eq	n_4 	 
+    cmp		x18, #5
+	b.eq	n_5 	 
+    cmp		x18, #6
+	b.eq	n_6	 
+    cmp		x18, #7
+	b.eq	n_7 	 
+    cmp		x18, #8
+	b.eq	n_8 	 
+    cmp		x18, #9
+	b.eq	n_9 	 
+    
